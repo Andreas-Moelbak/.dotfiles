98d3b6f56bf329bcf55e0bc64655c2e50b9a3b1f
Andreas Mølbak <anm@dubex.dk>
2024-05-01 12:52:25 +0200

    migrated norg to luarocks for version 8.0.0

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,40 +1,52 @@
 return {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = "nvim-neorg/neorg-telescope",
-    cmd = "Neorg",
-    ft = "norg",
-    opts = {
-        load = {
-            ["core.defaults"] = {},
-            ["core.ui"] = {},
-            ["core.ui.calendar"] = {},
-            ["core.tempus"] = {},
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- We'd like this plugin to load first out of the rest
+        config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+    },
+    {
+        "nvim-neorg/neorg",
+        --dependencies = { "luarocks.nvim" },
+        dependencies = {
+            "luarocks.nvim",
+            "nvim-neorg/neorg-telescope"
+        },
+        -- put any other flags you wanted to pass to lazy here!
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {},
+                    ["core.ui"] = {},
+                    ["core.ui.calendar"] = {},
+                    ["core.tempus"] = {},
 
-            ["core.dirman"] = {
-                config = {
-                    workspaces = {
-                        work = "~/notes/work",
-                        home = "~/notes/personal",
-                    }
-                },
-            },
+                    ["core.dirman"] = {
+                        config = {
+                            workspaces = {
+                                work = "~/notes/work",
+                                home = "~/notes/personal",
+                            }
+                        },
+                    },
 
-            ["core.integrations.telescope"] = {},
+                    ["core.integrations.telescope"] = {},
 
-            ["core.concealer"] = {},
-            ["core.completion"] = {
-                config = {
-                    engine = "nvim-cmp"
+                    ["core.concealer"] = {},
+                    ["core.completion"] = {
+                        config = {
+                            engine = "nvim-cmp"
+                        }
+                    },
+
+                    ["core.export"] = {},
+                    ["core.export.markdown"] = {
+                        config = {
+                            extensions = "all",
+                        },
+                    },
                 }
-            },
 
-            ["core.export"] = {},
-            ["core.export.markdown"] = {
-                config = {
-                    extensions = "all",
-                },
-            },
-        }
+            })
+        end,
     }
 }
